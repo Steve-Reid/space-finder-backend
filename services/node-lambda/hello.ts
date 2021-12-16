15e64eb6f60614624cca584d69fa72e23e97fc50
SteveReid <steve@appby.design>
2021-12-16 10:48:40 +0000

    set up local lambda debugging

--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,8 +1,16 @@
 import { v4 as uuid } from 'uuid';
+import { S3 } from 'aws-sdk';
+
+const s3Client = new S3();
+
 async function handler(event: any, context: any) {
+  const buckets = await s3Client.listBuckets().promise();
+  console.log('Got an event:');
+  console.log(event);
+
   return {
     statusCode: 200,
-    body: `Hello from Lambda ${uuid()}`,
+    body: `Hello, here are your buckets: ${JSON.stringify(buckets.Buckets)}`,
   };
 }
 
